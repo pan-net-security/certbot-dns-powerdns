17713afe659724ad22ce82973d3da89c603751b8
Diogenes S. Jesus <splashx@users.noreply.github.com>
2019-04-28 18:51:33 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 from setuptools import setup
 from setuptools import find_packages
 
-version = "0.1.0"
+version = "0.1.1"
 
 with open('README.md') as f:
     long_description = f.read()
