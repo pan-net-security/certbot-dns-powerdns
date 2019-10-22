1ecbe7eb87703f5e89540854627fe2335712c2fe
Diogenes Jesus <diogenes.jesus@telekom.com>
2019-10-22 21:41:41 +0200

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 from setuptools import setup
 from setuptools import find_packages
 
-version = "0.1.1"
+version = "0.2.0"
 
 with open('README.md') as f:
     long_description = f.read()
@@ -43,7 +43,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
